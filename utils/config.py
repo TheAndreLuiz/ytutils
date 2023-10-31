a7c8ffd714453a5384b63ff4531e1f668ee6860c
TheAndreLuiz <andreluizgaspar@edu.unifil.br>
2023-10-31 17:44:04 -0300

    update config methods and validation

--- utils/config.py
+++ utils/config.py
@@ -6,19 +6,22 @@ class Config:
 
 
     def __init__(self):
-        self.config = self.loadConfigFile()
+        pass
 
 
-    def get(self, config):
+    def getConfig(self):
+        return self.config
+
+
+    def getSingleConfig(self, config):
         return self.config[config]
 
 
     def _setConfig(self, config):
-        with open('config.ini', 'w') as configfile:
-            config.write(configfile)
+        self.config = config
 
 
-    def setDefaultConfig(self):
+    def setDefaultConfig(self): # TODO prettfy this
         config = configparser.ConfigParser()
         config['DEFAULT'] = {
             'showMsg':'True',
@@ -30,6 +33,8 @@ class Config:
             'showBadgedOnly':'False',
             'showMemberOnly':'False',
             'showBadgedNotMemberOnly':'False',
+            'ads':'False',
+            'didYouMean':'False',
             'msgFormat':'author badges | msg',
             'badgesFormat':'[badge]',
             'spacingFormat':'\n',
@@ -43,24 +48,43 @@ class Config:
             'chatUrl':'https://www.youtube.com/live_chat?v=',
             'msgUrl':'https://www.youtube.com/youtubei/v1/live_chat/send_message?key={}'.format(ytKey),
             'contChatUrl': 'https://www.youtube.com/youtubei/v1/live_chat/get_live_chat?key={}'.format(ytKey),
-            'ads':'False',
-            'didYouMean':'False',
         }
         self._setConfig(config)
 
 
-    def loadConfigFile(self):
-        with open('config.ini', 'r') as configFile:
-            config = configparser.ConfigParser()
-            config.read_file(configFile)
-            return config
+    def loadConfigIfOk(self):
+        config = self._readConfigFile(self)
+        if self._tryParseConfig(self, config):
+            self._setConfig(config)
+            return True
+        return False
 
 
     def updateConfigFile(self):
         pass
 
+    def _readConfigFile(self):
+        with open('config.ini', 'r') as configFile:
+            config = configparser.ConfigParser()
+            config.read_file(configFile)
+            return config
 
-    def checkConfigFile(self):
-        if os.path.isfile('config.ini'):
-            return True
-        return False
+            
+    def tryParseConfig(self, config):
+        config = config['DEFAULT']
+        return config['showMsg'] in ['True', 'False'] and \
+            config['showPoll'] in ['True', 'False'] and \
+            config['showAuthor'] in ['True', 'False'] and \
+            config['showBadges']in ['True', 'False'] and \
+            config['showBanner'] in ['True', 'False'] and \
+            config['showWarning'] in ['True', 'False'] and \
+            config['showBadgedOnly'] in ['True', 'False'] and \
+            config['showMemberOnly'] in ['True', 'False'] and \
+            config['showBadgedNotMemberOnly'] in ['True', 'False'] and \
+            config['ads'] in ['True', 'False'] and \
+            config['didYouMean'] in ['True', 'False'] and \
+            'author' in config['msgFormat'] or 'badges' in config['msgFormat'] or 'msg' in config['msgFormat'] and \
+            'badge' in config['badgesFormat'] and \
+            config['msgColor'] is not '' and \
+            config['modNameColor'] is not '' and \
+            config['membersNameColor'] is not ''
